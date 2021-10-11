1061f1b1c6b66fa593cdb485be7f36e8d8fad95b
Simon Bueschges <simon.bueschges@stat-up.com>
2021-10-11 15:31:19 +0200

    Initial commit

---
+++ Code/global.R
@@ -0,0 +1,21 @@
+#*************************************************************
+## Dies ist die Global zum R Schulungsprojekt bei Burdaforward 
+#*************************************************************
+
+# Libraries sourcen ----
+
+source("Code/libraries.R", encoding = "UTF-8")
+
+
+# Daten laden ----
+
+# Covid <- 
+# read.csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+
+# Daten aufbereiten ----
+
+Covid
+
+# Daten visualisieren ----
+
+# Report erstellen ----
---
+++ Code/libraries.R
@@ -0,0 +1,11 @@
+# Libraries Calls ----
+
+## Datenmanipulation ----
+library(dplyr)
+library(stringr)
+
+## Daten einlesen ----
+library(readr)
+
+## Grafiken erstellen----
+library(ggplot2)
