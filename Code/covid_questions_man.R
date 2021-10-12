963a07d5147e0ccbb3f240fe297f74501486c492
FOL-MN <92360729+FOL-MN@users.noreply.github.com>
2021-10-12 12:53:58 +0200

    Hier meine Lösungen, VG man

---
+++ Code/covid_questions_man.R
@@ -0,0 +1,83 @@
+# Allgemeine Übung -------------------------------------------------------------
+
+# Erhöhe jeden zweiten Wert des Vektors um das doppelte
+# des vorherigen Wert im Vektor (1:10)
+
+ergebnis <- c()
+x = 1:20
+for (y in x) {
+  if (y %% 2 == 0) {
+   ergebnis[y] <- (y+x[y-1]*2)
+  } else {
+    ergebnis[y]<-(y)
+  }
+}
+ergebnis
+
+## Erzeuge einen Vektor der aufsteigenden Zahlen an den Positionen der
+## Vokale im letters Vektor enthält und 0 für Konsonaten.
+## Z.B. c("a", "g", "e", "f", "a")
+## wird zu c(1, 0, 2, 0, 3)
+
+ergebnis2 <- c()
+x <- c("a","c","f","v","e","f","p","a")
+for (y in 1:length(x)) {
+  print(ergebnis2)
+  print(cumsum(ergebnis2))
+  print("\n")
+  if (x[y]%in% c("a","e","o","i","u")) {
+    ergebnis2[y] <- 1 
+  } else {
+    ergebnis2[y] <- 0
+  }
+}
+ergebnis2
+
+
+
+# Fragen zu Covid Daten ----
+
+## Wie viele Covid Cases gab es bis zum Zeitpunkt
+## an dem die erste Impfung in Asien
+## verabreicht wurde ?
+
+covtable_2 <- Covid %>% 
+  select("location", "date", "total_cases", "new_cases", "total_vaccinations") %>% 
+    filter(location == "Asia") %>% 
+      mutate(date = as.Date(date)) %>% 
+        arrange(date) %>% 
+          filter(total_vaccinations == min(total_vaccinations, na.rm = TRUE ))
+
+Covid <- mutate (Covid, date = as.Date(date))
+covtable_2 %>%
+    left_join(Covid) %>% 
+    View()
+
+
+
+## Was ist das Maximum an Impfungen je Land an einzelnem Tag?
+## An welchem Tag trat dieses Maximum auf?
+## Welchen Anteil macht dieser Wert von den Gesamtimpfungen des Kontients
+## an dem zugehörigen Tag aus?
+
+
+
+
+## Berechne den median der neuen Fälle und filter dann alle Zeilen,
+## bei denen es in 2021 mehr neue Fälle als den Median gab.
+## Ordne die in absteigender Reihenfolge und berechne den Anstieg / Abstieg
+## in Bezug auf den Vortag je Land
+
+
+## In welchem Kontinent gibt es die meisten Länder, deren HDI geringer als 0,549
+## ist? (Geringe Entwicklung)
+
+## Zwischen welchen beiden Tagen war je Land
+## der größte Unterschied an neuen Fallzahlen zu beobachten?
+
+
+## Kontrolliere, ob es zu jeder Datum und Land Kombination nur einen Eintrag gibt
+
+
+## Welcher %-Anteil der Geimpften in Frankreich ist am 24.05.2021 vollständig
+## geimpft?
